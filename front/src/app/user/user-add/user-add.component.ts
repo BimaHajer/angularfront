af024d8e4102b05bcf2991d5f3c401a5f1fe2050
unknown <bima.tn.dev.h.f@gmail.com>
2025-02-11 09:39:34 +0100

    user module

--- front/src/app/user/user-add/user-add.component.ts
+++ front/src/app/user/user-add/user-add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 @Component({
@@ -6,28 +6,63 @@ import { UserService } from '../user.service';
   templateUrl: './user-add.component.html',
   styleUrl: './user-add.component.css'
 })
-export class UserADDComponent {
-  form:FormGroup
-  show: boolean=false;
-  msg: string="";
-  constructor( private fb:FormBuilder , private userService:UserService) { 
-    this.form=this.fb.group({ 
-      firstName:['',[Validators.required,Validators.minLength(3)]],
-      lastName:['',[Validators.required,Validators.minLength(3)]],
-      email:['',[Validators.required,Validators.email]],
-      password:['',Validators.required],
-      role:['']
-    })
+export class UserADDComponent implements OnInit {
+  currentStep: number = 1;
+  maxSteps: number = 3;
+  isFinalStep: boolean = false;
 
-  }
-  submit(){
-  this.userService.createUser(this.form.value).subscribe(res=>{
-    console.log(res)
-    this.show=true
-    this.msg="User added successfully"
-    this.form.reset()
-  })
+  userForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.userForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      role: [''],
+      created_at: [''],
+      updated_at: [''],
+      deleted_at: [''],
+    });
   }
 
+  ngOnInit(): void {
+    // Initialization logic if necessary
+  }
+
+  goToPreviousStep(): void {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  goToNextStep(): void {
+    if (this.currentStep < this.maxSteps) {
+      this.currentStep++;
+    }
+  }
+
+  finishStepper(): void {
+    this.isFinalStep = true;
+  }
+
+  isStepSuccess(step: number): boolean {
+    // Add your logic to determine if a step is successful
+    return false;
+  }
+
+  isStepCompleted(step: number): boolean {
+    // Add your logic to determine if a step is completed
+    return false;
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+      console.log(this.userForm.value);
+    } else {
+      console.log('Form is invalid'); 
+    }
+
+}
 
 }
